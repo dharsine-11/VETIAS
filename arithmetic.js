dd869ac7e0360029f765a220ac2b02e80f2006a2
dharsine-11 <dharsine2007@gmail.com>
2026-06-12 11:40:24 +0530

    Added comparison

--- arithmetic.js
+++ arithmetic.js
@@ -31,3 +31,12 @@ console.log(a *= 5)//multiply & assign
 console.log(a /= 5)//divide & assign
 console.log(a %= 5)//modulus & assign
 console.log(a **= 5)//exponentiation & assign
+console.log("Comparison Operator")
+var a = 10
+var b = 2
+console.log(a==b)//equal to
+console.log(a!=b)//not equal to
+console.log(a>b)//greater than
+console.log(a<b)//less than
+console.log(a>=b)//greater than or equal to
+console.log(a<=b)//less than or equal to 
