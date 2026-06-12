02ad75ad5298c873749f16ac3897c79d616aef6a
dharsine-11 <dharsine2007@gmail.com>
2026-06-12 11:00:01 +0530

    Added Arithmetic

---
+++ arithmetic.js
@@ -0,0 +1,25 @@
+//Arithmetic Operators
+//Addition
+var a = 4
+var b = 4
+console.log(a+b)
+//Subtraction
+var a = 5
+var b = 3
+console.log(a-b)
+//Multiplication
+var a = 4
+var b = 4
+console.log(a*b)
+//Division
+var a = 10
+var b = 2
+console.log(a/b)
+//Modulus
+var a = 10
+var b = 3
+console.log(a%b)
+//Exponentiation
+var a = 2
+var b = 3
+console.log(a**b)
